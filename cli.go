62cc1a12847e340c4e8094e44b5866ffaa691d37
shiimaxx <yoshima@shiimaxx.com>
2018-05-09 22:19:18 +0900

    Add cli flag and it tests

--- cli.go
+++ cli.go
@@ -1,6 +1,11 @@
 package main
 
-import "io"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 // Exit codes are int values that represent an exit code for a particular error.
 const (
@@ -17,5 +22,37 @@ type CLI struct {
 
 // Run invokes the CLI with the given arguments.
 func (c *CLI) Run(args []string) int {
+	var (
+		output  string
+		version bool
+	)
+
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.SetOutput(c.outStream)
+
+	flags.StringVar(&output, "output", "./", "output file")
+	flags.StringVar(&output, "o", "./", "output file(Short)")
+	flags.BoolVar(&version, "version", false, "print version information")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	if version {
+		fmt.Fprintf(c.outStream, "%s version %s\n", Name, Version)
+		return ExitCodeOK
+	}
+
+	if len(flags.Args()) < 1 {
+		fmt.Fprintln(c.errStream, "missing arguments")
+		return ExitCodeError
+	}
+
+	filePath := flags.Args()[0]
+	if _, err := os.Stat(filePath); os.IsExist(err) {
+		fmt.Fprintf(c.errStream, "%s: already exits\n", filePath)
+		return ExitCodeError
+	}
+
 	return ExitCodeOK
 }
